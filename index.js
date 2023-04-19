51e8b6acd9eeba61549a83451a38c9be713baeec
arohyadav <37497802+arohyadav@users.noreply.github.com>
2023-04-19 15:53:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,69 +11,69 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define Sequelize database connection
 
-var sequelize = new Sequelize('postgres://postgres:arohyadav@localhost:5432/postgres');
-const testconnection = async(req,res) => {
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.log('Unable to connect to the database:', error);
-}
-}
+// var sequelize = new Sequelize('postgres://postgres:arohyadav@localhost:5432/postgres');
+// const testconnection = async(req,res) => {
+// try {
+//   await sequelize.authenticate();
+//   console.log('Connection has been established successfully.');
+// } catch (error) {
+//   console.log('Unable to connect to the database:', error);
+// }
+// }
 
-// Define a model for the form data
-const Form1 = sequelize.define('Form1', {
-  fullname: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  workexperience: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  jobtitle: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  phonenumber: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+// // Define a model for the form data
+// const Form1 = sequelize.define('Form1', {
+//   fullname: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   email: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   workexperience: {
+//     type: DataTypes.TEXT,
+//     allowNull: false,
+//   },
+//   jobtitle: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   phonenumber: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+// });
 
-// Create the table in the database
-Form1.sync();
+// // Create the table in the database
+// Form1.sync();
 
-// Define a route for handling form submissions
-app.post('/submit-form', async (req, res) => {
-    try {
-      // Extract form data from request body
-      const { fullname, email, workexperience, jobtitle, phonenumber } = req.body;
+// // Define a route for handling form submissions
+// app.post('/submit-form', async (req, res) => {
+//     try {
+//       // Extract form data from request body
+//       const { fullname, email, workexperience, jobtitle, phonenumber } = req.body;
   
-      // Create new User instance with form data
-      const user = await Form1.create({
-        fullname,
-        email,
-        workexperience,
-        jobtitle,
-        phonenumber
-      });
+//       // Create new User instance with form data
+//       const user = await Form1.create({
+//         fullname,
+//         email,
+//         workexperience,
+//         jobtitle,
+//         phonenumber
+//       });
   
-      // Send success response
-      res.status(200).json({
-        message: 'Form submitted successfully',
-        user,
-      });
-    } catch (error) {
-      // Handle error
-      console.log(error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  });
+//       // Send success response
+//       res.status(200).json({
+//         message: 'Form submitted successfully',
+//         user,
+//       });
+//     } catch (error) {
+//       // Handle error
+//       console.log(error);
+//       res.status(500).json({ message: 'Internal server error' });
+//     }
+//   });
   
 //   app.use(express.static('client'));
 
